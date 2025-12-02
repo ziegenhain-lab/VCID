387912d22be5fbc88e10cb6086353f0465a8fa85
Zhouhui <zhouhui.qi@stud.ki.se>
2025-12-02 15:38:07 +0100

    Eng anno for SNP_calling folder

--- algorithem/snakemake_pipeline/SNP_calling/extract_barcodes_and_split_bam.sh
+++ algorithem/snakemake_pipeline/SNP_calling/extract_barcodes_and_split_bam.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# 检查参数
 if [ "$#" -ne 2 ]; then
-    echo "用法: $0 barcode_annotation.txt 输出目录"
+    echo "Usage: $0 barcode_annotation.txt Output_Directory"
     exit 1
 fi
 
-# 读取参数
 barcode_file="$1"
 output_dir="$2"
 
-# 创建输出目录（如果不存在）
 mkdir -p "$output_dir"
 
-# 自动检测 Sort_Population 或 sort_population 列号（忽略大小写）
+# Automatically detects column numbers in Sort_Population or sort_population (ignoring case).
 sort_col=$(awk -F'\t' 'NR==1 {
     for(i=1; i<=NF; i++) {
         colname = tolower($i)
@@ -24,38 +21,36 @@ sort_col=$(awk -F'\t' 'NR==1 {
     }
 }' "$barcode_file")
 
-echo "Sort_Population 列号: $sort_col"
+echo "Sort_Population column number: $sort_col"
 
-
-# 确保找到了 Sort_Population 列
 if [ -z "$sort_col" ]; then
-    echo "错误：未找到 Sort_Population 列"
+    echo "Error: Sort_Population column not found"
     exit 1
 fi
 
-# 获取所有唯一的 Sort Population 值
+# Get all unique Sort Population values
 sort_values=$(awk -F'\t' -v col="$sort_col" 'NR>1 {print $col}' "$barcode_file" | sort -u)
 
-# 获取 XC_DNBPE 所在的列号
+# Get the column number where XC_DNBPE is located.
 col_num=$(awk -F'\t' 'NR==1 {for(i=1;i<=NF;i++) if($i=="XC_DNBPE") print i}' "$barcode_file")
 
-# 确保找到 XC_DNBPE 列
+# Ensure the XC_DNBPE column is found.
 if [ -z "$col_num" ]; then
-    echo "错误：未找到 XC_DNBPE 列"
+    echo "Error: XC_DNBPE column not found"
     exit 1
 fi
 
-# 遍历每个 Sort Population 值，进行数据处理
+# Iterate through each Sort Population value and perform data processing.
 for value in $sort_values; do
-    # 生成文件名
+
     subset_file="$output_dir/${value}_subset.txt"
     output_file="$output_dir/${value}_subset_cellBClist.txt"
 
-    # 提取对应的行存入 subset_file
+    # Extract the corresponding rows and store them in subset_file
     awk -F'\t' -v val="$value" -v col="$sort_col" '$col == val' "$barcode_file" > "$subset_file"
 
-    # 提取 XC_DNBPE 列，并存入 output_file
+    # Extract the XC_DNBPE column and save it to the output file.
     awk -F'\t' -v col="$col_num" '{print $col}' "$subset_file" > "$output_file"
 
-    echo "已生成: $output_file"
+    echo "Generated: $output_file"
 done
